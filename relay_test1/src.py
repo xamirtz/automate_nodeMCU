0dbc12eb063fa8920e76b4eed5be40417f83c9b3
Naman Chauhan <chauhannaman98@gmail.com>
2019-02-09 11:31:36 +0530

    Add Python files

---
+++ relay_test1/src.py
@@ -0,0 +1,5 @@
+from machine import Pin
+from machine import Timer
+
+# doing
+
--- testNodeMCU/srcCode.py
+++ testNodeMCU/srcCode.py
@@ -3,6 +3,6 @@ from machine import Timer
 
 p_test = Pin(4, Pin.OUT)
 p_test.on()
-time.sleep(1);
+time.sleep(1)
 p_test.off()
-time.sleep(1);
+time.sleep(1)
